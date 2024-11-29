0b683deb10518d220ff0c13c66d9f9b4d2baaf9f
231189alo <231189@ids.upchiapas.edu.mx>
2024-11-29 01:43:41 -0600

    Creacion del AuthGuard para verificar la expiracion token

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -3,5 +3,13 @@ import { CanActivateFn, Router } from '@angular/router';
 import { AuthServiceService } from '../auth/services/auth-service.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthServiceService);
+  const router = inject(Router);
+
+  if (authService.isTokenExpired()) {
+    router.navigate(['/login']);
+    return false;
+  }
+
   return true;
 };
